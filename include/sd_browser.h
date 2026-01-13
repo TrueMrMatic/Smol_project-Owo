792bfc4c638546ebf05d6906ea8f3b87c0592969
Mr_Matic <86529403+TrueMrMatic@users.noreply.github.com>
2026-01-13 17:00:51 +0100

    Delete include directory

--- include/sd_browser.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define SD_NAME_MAX 256
-
-int sd_list_swfs(const char* dir, char names[][SD_NAME_MAX], int max_files);
--- include/swf_info.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void swf_show_header(const char* fullpath);
